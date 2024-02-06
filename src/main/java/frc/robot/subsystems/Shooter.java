48c0b5fcc0886eb63e9c5f99e40c8ccf8de3b349
carolinemerkle <caroline.merkle@credohigh.org>
2024-02-05 17:15:48 -0800

    add ultrasonic sensor

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,18 +2,24 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase{
+    //shooter motor controllers
     private final CANSparkMax m_shooterLeft = new CANSparkMax(ShooterConstants.SHOOTER_LEFT, MotorType.kBrushless);
     private final CANSparkMax m_ShooterRight = new CANSparkMax(ShooterConstants.SHOOTER_RIGHT, MotorType.kBrushless);
-
-
-
+    //ultrasonic sensor 
+    private final Ultrasonic m_distanceSensor = new Ultrasonic(null, null);
+    //get the distance from the range sensor in milimeters 
+    public double distanceMilimeters = m_distanceSensor.getRangeMM();
+    
+    //runs the shooter
     public void runShooter(double leftSpeed, double rightSpeed){
-        m_shooterLeft.set(ShooterConstants.SHOOTER_SPEED_LEFT);
-        m_ShooterRight.set(ShooterConstants.SHOOTER_SPEED_RIGHT);
+        m_shooterLeft.set(leftSpeed);
+        m_ShooterRight.set(rightSpeed);
     }
 
 
