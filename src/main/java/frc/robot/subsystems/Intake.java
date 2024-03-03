edf28e98f63e7fcde7c314e3571f80dee31889e2
carolinemerkle <caroline.merkle@credohigh.org>
2024-03-03 09:05:47 -0800

    mmmm

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,6 +4,7 @@ import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
@@ -15,43 +16,43 @@ public class Intake extends SubsystemBase {
     //Beam breaks
     private final DigitalInput m_IntakeBB1 = new DigitalInput(IntakeConstants.BEAM_BREAK_INTAKE_ID1);
   
-        
-
-
-//extends intake over the bumpers using pnumatics
-public void extendIntake(){
-    m_solenoid.set(DoubleSolenoid.Value.kForward);
-   
-}
-//retract intake into starting position using pnumatics
-public void retractIntake(){
-    m_solenoid.set(DoubleSolenoid.Value.kReverse);
-   
-}
-//turns off the intake pnumatics
-public void intakeOff(){
-    m_solenoid.set(DoubleSolenoid.Value.kOff);
+    //extends intake over the bumpers using pnumatics
+    public void extendIntake(){
+        m_solenoid.set(DoubleSolenoid.Value.kForward);
     
-}
+    }
+    //retract intake into starting position using pnumatics
+    public void retractIntake(){
+        m_solenoid.set(DoubleSolenoid.Value.kReverse);
+    
+    }
+    //turns off the intake pnumatics
+    public void intakeOff(){
+        m_solenoid.set(DoubleSolenoid.Value.kOff);
+        
+    }
 
-public boolean beamBroken(){
-    return m_IntakeBB1.get();
-}
+    public boolean beamBroken(){
+        return m_IntakeBB1.get();
+    }
 
-public BooleanSupplier m_BooleanSupplier(){
-    return (() -> m_IntakeBB1.get() == false);
-}
-public void getAsBoolean(){
-    m_BooleanSupplier().getAsBoolean();
-}
+    public BooleanSupplier m_BooleanSupplier(){
+        return (() -> m_IntakeBB1.get() == false);
+    }
+
+    public void getAsBoolean(){
+        m_BooleanSupplier().getAsBoolean();
+    }
 
 
-@Override
-public void periodic(){
-    m_BooleanSupplier();
-    System.out.println(m_BooleanSupplier());
-    System.out.println("beam break get " + beamBroken());
-}
+    @Override
+    public void periodic(){
+        m_BooleanSupplier();
+        System.out.println(m_BooleanSupplier());
+        System.out.println("beam break get " + beamBroken());
 
-}
+        SmartDashboard.putBoolean("Note in intake", !beamBroken());
+    }
+
+    }
 
